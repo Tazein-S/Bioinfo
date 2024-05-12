6c3900fe7fd44b5e34cc095c5ccc9362c57a06ed
Tazein-S <tazeinshah91@gmail.com>
2024-05-11 20:43:23 -0400

    updated 5/11 with more problems solved

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,23 +1,49 @@
 ### Created on 4/29/24 by Tazein Shah
 ### Based off the tutorial made by rebelScience on Youtube
 
-#importing necessary packages
-import collections
-
 #define the nucleotides
-Nucleotides = ["A", "C", "G", "T"]
+nucleotides = ["A", "C", "G", "T"]
 
 # checking to see if the sequence we have is a DNA string
 def validateSed(dna_seq):
-  tmpseq = dna_seq.upper() #we need them to be uppercase because of the list we defined earlier
-  for nuc in tmpseq:
-    if nuc not in Nucleotides:
-      return False
-  return tmpseq
+    tmpseq = dna_seq.upper() #we need them to be uppercase because of the list we defined earlier
+    for nuc in tmpseq:
+        if nuc not in nucleotides:
+            return False
+    return tmpseq
 
 def countNucFrequency(dna_seq):
-  return(dict(collections.Counter(dna_seq)))
-  #tmpFreqDict = {nuc: 0 for nuc in Nucleotides} #setting up a dict where every value starts as 0
-  #for nuc in dna_seq:
-    #tmpFreqDict[nuc] += 1
-  #return tmpFreqDict
+    tmpFreqDict = {nuc: 0 for nuc in Nucleotides} #setting up a dict where every value starts as 0
+    for nuc in dna_seq:
+        tmpFreqDict[nuc] += 1
+    return tmpFreqDict
+
+def dnaTranscribe(dna_seq):
+    tmpseq = dna_seq.upper() #we need them to be uppercase because of the list we defined earlier
+    tmpseq = tmpseq.replace('T','U')
+    return tmpseq
+
+def complementingDNA(dna_seq):
+    tmpseq = dna_seq.upper() #we need them to be uppercase because of the list we defined earlier    
+    reverse_seq = tmpseq[::-1] #reverse the sequence
+    comp_dna = '' #create an empty string
+    
+    for nuc in reverse_seq:
+        if nuc == 'A':
+            comp_dna += str('T')
+            
+        if nuc == 'T':
+            comp_dna += str('A')
+            
+        if nuc == 'C':
+            comp_dna += str('G')
+            
+        if nuc == 'G':
+            comp_dna += str('C')
+    
+    return comp_dna
+
+            
+
+                   
+            
